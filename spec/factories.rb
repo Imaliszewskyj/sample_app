37a811563dc6f645be306afff9697772115d0d54
Ian <imaliszewskyj@gmail.com>
2012-06-27 15:01:12 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Ian Maliszewskyj"
+    email    "imaliszewskyj@fenestra.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
